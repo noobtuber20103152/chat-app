4f0824ad8463b495b5b09eba93ef19e8d3fd0e82
ujjawal <ujjawalrachhoya100@gmail.com>
2022-12-16 14:31:29 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    APP_ID: "1:58406844821:web:e0ae99c5801d3b51de1e9c",
+    MESSAGING_SENDER_ID: "58406844821",
+    STORAGE_BUCKET: "chatapp-f9764.appspot.com",
+    PROJECT_ID: "chatapp-f9764",
+    AUTH_DOMAIN: "chatapp-f9764.firebaseapp.com",
+    API_KEY: "AIzaSyDiJpIroUmFxNUhotk66vMIS1_qHSq-_Tg",
+  },
+};
